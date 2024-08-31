657eb7dd3c9903c5989319a532db8cebdde74bc2
jhm <maxosen64@gmail.com>
2024-08-31 12:14:46 +0800

    add continuous integration Actions workflow

---
+++ frontend-admin/vitest.config.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    // ...
+    reporters: process.env.GITHUB_ACTIONS
+      ? ["html", "github-actions", "verbose"]
+      : ["verbose"],
+    outputFile: "./test-output/index.html",
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src/"),
+      "@api-contract": path.resolve(__dirname, "./src/api-contract"),
+      "@config": path.resolve(__dirname, "./src/config"),
+    },
+  },
+});
